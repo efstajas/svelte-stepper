7b5e0962a40f343fb41ac4b40870e620d19bab92
Georgios Jason Efstathiou <j@jason-e.dev>
2023-11-27 21:04:57 +0100

    format with spaces instead of tabs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,34 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-		'plugin:svelte/recommended'
-	],
-	plugins: ['@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			},
-			rules: {
-				'no-undef': 'off'
-			}
-		}
-	],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:svelte/recommended'
+  ],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'no-undef': 'off'
+      }
+    }
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  }
 };
--- src/lib/step-controller.ts
+++ src/lib/step-controller.ts
@@ -2,33 +2,33 @@ import { createEventDispatcher } from 'svelte';
 import type { StepComponentEvents, Steps } from './types';
 
 export default () => {
-	const dispatch = createEventDispatcher<StepComponentEvents>();
+  const dispatch = createEventDispatcher<StepComponentEvents>();
 
-	function nextStep() {
-		dispatch('goForward');
-	}
+  function nextStep() {
+    dispatch('goForward');
+  }
 
-	function previousStep() {
-		dispatch('goBackward');
-	}
+  function previousStep() {
+    dispatch('goBackward');
+  }
 
-	function move(by: number) {
-		dispatch('goForward', { by });
-	}
+  function move(by: number) {
+    dispatch('goForward', { by });
+  }
 
-	function sidestep(steps: Steps) {
-		dispatch('sidestep', { steps });
-	}
+  function sidestep(steps: Steps) {
+    dispatch('sidestep', { steps });
+  }
 
-	function cancelSidestep() {
-		dispatch('cancelSidestep');
-	}
+  function cancelSidestep() {
+    dispatch('cancelSidestep');
+  }
 
-	return {
-		nextStep,
-		previousStep,
-		move,
-		sidestep,
-		cancelSidestep
-	};
+  return {
+    nextStep,
+    previousStep,
+    move,
+    sidestep,
+    cancelSidestep
+  };
 };
